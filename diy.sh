784a814666eb502400556af8f6aeae285a6cab9e
8688Add <42204083+8688Add@users.noreply.github.com>
2020-02-26 06:10:46 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,10 +6,10 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a
